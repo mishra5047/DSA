44967b743de4b77628c45f97e74cb9fbe84ff8a0
Abhishek Mishra <43989650+mishra5047@users.noreply.github.com>
2020-04-09 18:52:19 +0530

    Second

--- src/com/abhishek/Queue/Main.java
+++ src/com/abhishek/Queue/Main.java
@@ -14,7 +14,13 @@ public class Main {
 //    System.out.println(queue.deque());
 //    System.out.println(queue);
 
-
+    PriorityQueue queue =  new PriorityQueue();
+    queue.insert(5);
+    queue.insert(3);
+    queue.insert(6);
+    queue.insert(1);
+    queue.insert(4);
+    System.out.println(queue);
     }
 
 
